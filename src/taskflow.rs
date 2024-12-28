23e7014cd1719d06983c935559582c5ab4a7a2aa
ashyanSpada <ashyan_spada@outlook.com>
2024-12-28 15:27:19 +0800

    feat: update

--- src/taskflow.rs
+++ src/taskflow.rs
@@ -130,3 +130,91 @@ impl<'a, T: State<T> + Clone> IfTask<'a, T> {
         self
     }
 }
+
+pub struct TaskImpl<'a, T: State<T> + Clone> {
+    n: &'a str,
+    method: &'a dyn Fn(T) -> Result<T, Error>,
+}
+
+impl<'a, T: State<T> + Clone> Task<T> for TaskImpl<'a, T> {
+    fn name(&self) -> &str {
+        self.n
+    }
+    fn execute(&self, state: T) -> Result<T, Error> {
+        (self.method)(state)
+    }
+}
+
+pub fn new_task<'a, T: State<T> + Clone>(
+    n: &'a str,
+    method: &'a dyn Fn(T) -> Result<T, Error>,
+) -> TaskImpl<'a, T> {
+    TaskImpl {
+        n: n,
+        method: method,
+    }
+}
+
+pub fn sequence<'a, T: State<T> + Clone>(tasks: Vec<&'a dyn Task<T>>) -> SequenceTask<'a, T> {
+    SequenceTask {
+        n: "".to_string(),
+        tasks: tasks,
+    }
+}
+
+pub fn or<'a, T: State<T> + Clone>(tasks: Vec<&'a dyn Task<T>>) -> OrTask<'a, T> {
+    OrTask {
+        n: "".to_string(),
+        tasks: tasks,
+    }
+}
+
+mod test {
+    use super::*;
+    #[derive(Debug)]
+    struct TestState {
+        num: i32,
+    }
+    impl State<TestState> for TestState {
+        fn merge(&self, b: &TestState) -> TestState {
+            TestState { num: self.num }
+        }
+    }
+    impl Clone for TestState {
+        fn clone(&self) -> Self {
+            TestState { num: self.num }
+        }
+    }
+
+    #[test]
+    fn test_sequence() {
+        let task1 = new_task("task1", &|a: TestState| -> Result<TestState, Error> {
+            Ok(TestState { num: a.num + 1 })
+        });
+        let task2 = new_task("task1", &|a: TestState| -> Result<TestState, Error> {
+            Ok(TestState { num: a.num + 2 })
+        });
+        let task3 = new_task("task1", &|a: TestState| -> Result<TestState, Error> {
+            Ok(TestState { num: a.num + 3 })
+        });
+        let seq_task = sequence(vec![&task1, &task2, &task3]);
+        let res = seq_task.execute(TestState { num: 100 });
+        print!("{:?}", res);
+    }
+
+    #[test]
+    fn test_or() {
+        let task1 = new_task("task1", &|a: TestState| -> Result<TestState, Error> {
+            Ok(TestState { num: a.num + 1 })
+        });
+        let task2 = new_task("task1", &|a: TestState| -> Result<TestState, Error> {
+            Ok(TestState { num: a.num + 2 })
+        });
+        let task3 = new_task("task1", &|a: TestState| -> Result<TestState, Error> {
+            Ok(TestState { num: a.num + 3 })
+        });
+        let or_task = or(vec![&task1, &task2, &task3]);
+        let res = or_task.execute(TestState { num: 100 });
+        print!("{:?}", res);
+    }
+}
